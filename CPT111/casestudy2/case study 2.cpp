4294cf212bd19ad753b514af6b2f3e96e1effb12
Chi Hao <81066169+haocloo@users.noreply.github.com>
2022-11-06 23:00:59 +0800

    havent read finish question

---
+++ CPT111/casestudy2/case study 2.cpp
@@ -0,0 +1,14 @@
+suggest best 3 devices
+if none, at least 1 device
+
+match min 3 factors
+can have 1 factor only (user)
+
+order preferences + important
+
+< ceiling value, additional stuff
+
+i.Model
+ii.Make
+iii.Price
+iv.Processor

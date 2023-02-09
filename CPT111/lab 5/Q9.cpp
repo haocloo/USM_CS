ad2a354d53f79e78aeb66eaedd89fada71fdf6cd
Loo Chi Hao <81066169+haocloo@users.noreply.github.com>
2023-02-09 17:34:11 +0800

    Update Q9.cpp

--- CPT111/lab 5/Q9.cpp
+++ CPT111/lab 5/Q9.cpp
@@ -1,43 +1,30 @@
 #include <iostream>
 using namespace std;
 int main(){
-    char firstC, secondC;
-    int firstnum, secondnum, rows, columns, counter = 0;
-
-    cout << "Enter a character : ";
-    cin >> firstC;
-    cout << "Number of times to repeat character : ";
-    cin >> firstnum;
-    cout << "Enter another character : ";
-    cin >> secondC;
-    cout << "Number of times to repeat character : ";
-    cin >> secondnum;
-    cout << "Number of rows : ";
-    cin >> rows;
-    cout << "Number of columns : ";
-    cin >> columns;
-
-    
-    while true{
-        dimension = rows*columns;
-        while(elements <= dimension)
-            for (int i = 0 ; i < firstnum ; i++)            
-                if(elements > columns)
-                    cout << endl << firstC;
-                else
-                    cout << firstC;
-                elements++;
-
-        if(elements > dimension)
-            break;
-        
-        for (int i = 0 ; i < secondnum ; i++)            
-            if(elements > columns)
-                cout << endl << secondC;
-            else
-                cout << secondC;
-            elements++;
-    }
-    return 0;
+	char c1, c2;
+	int n1, n2, counter=0, row, col;
+	cin>>c1>>n1>>c2>>n2>>row>>col;
+	
+	for(int i=0; i<(row*col); i++){
+		if(counter<n1){
+			// if counter = 0 to n1, print c1, add counter after printed
+			cout<<c1;
+			counter++;
+		} 
+		else if(counter<(n1+n2)){
+			// if counter = n1 to n1+n2, print c2, add counter after printed
+			cout<<c2;
+			counter++;
+		} 
+		if(counter == n1+n2){
+			// reset the counter if already printed n1+n2 characters
+			counter=0;
+		} 
+		
+		if(i%col == col-1) {
+			// handle characters per line
+			cout<<endl;
+		}
+	}
 }
 

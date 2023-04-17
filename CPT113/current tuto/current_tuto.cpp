5c24ec249fbef6cd394303cccb750d3fd04bd9c3
Chi Hao <81066169+haocloo@users.noreply.github.com>
2023-04-17 21:56:33 +0800

    my week 3 and 4 ans

--- CPT113/current tuto/current_tuto.cpp
+++ CPT113/current tuto/current_tuto.cpp
@@ -1,11 +1,13 @@
-// Tuto W2 : Topic : Struct, Enum, Class
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <fstream>
 using namespace std;
+
+// --------------------------------------------- Tuto W1 ---------------------------------------------
+
+// Tuto W2 : Topic : Struct, Enum, Class
 // W2 Q1a
-/*
 struct Student{
     string name;
     int ID;
@@ -17,25 +19,25 @@ struct Student{
 int main(){
     Student stud;
     // W2 Q1a only
-    // cout << "Enter name: ";
-    // cin >> stud.name;
-    // cout << "Enter ID: ";
-    // cin >> stud.ID;
-    // cout << "Enter Desasiswa: ";
-    // cin >> stud.Desasiswa;
-    // cout << "Enter Year: ";
-    // cin >> stud.Year;
-    // cout << "Enter Semester: ";
-    // cin >> stud.Semester;
-    // cout << "Enter CGPA: ";
-    // cin >> stud.CGPA;
+    cout << "Enter name: ";
+    cin >> stud.name;
+    cout << "Enter ID: ";
+    cin >> stud.ID;
+    cout << "Enter Desasiswa: ";
+    cin >> stud.Desasiswa;
+    cout << "Enter Year: ";
+    cin >> stud.Year;
+    cout << "Enter Semester: ";
+    cin >> stud.Semester;
+    cout << "Enter CGPA: ";
+    cin >> stud.CGPA;
 
-    // cout << "Name: " << stud.name << endl;
-    // cout << "ID: " << stud.ID << endl;
-    // cout << "Desasiswa: " << stud.Desasiswa << endl;
-    // cout << "Year: " << stud.Year << endl;
-    // cout << "Semester: " << stud.Semester << endl;
-    // cout << "CGPA: " << stud.CGPA << endl;
+    cout << "Name: " << stud.name << endl;
+    cout << "ID: " << stud.ID << endl;
+    cout << "Desasiswa: " << stud.Desasiswa << endl;
+    cout << "Year: " << stud.Year << endl;
+    cout << "Semester: " << stud.Semester << endl;
+    cout << "CGPA: " << stud.CGPA << endl;
 
     // W2 Q1b
     // array of structs
@@ -71,10 +73,9 @@ int main(){
     return 0;
 }
 
-*/
+
 
 // W2 Q2
-/*
 enum Day { MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY };
 // enum Day MONDAY will get the value 0
 
@@ -97,10 +98,9 @@ int main(){
         << fixed << total << endl;
     return 0;
 }
-*/
+
 
 // W2 Q3
-/*
 class Student{
     private:
         string name, desasiswa;
@@ -174,10 +174,9 @@ int main(){
     cout << "Semester : " << student1.getSem() << endl;
     cout << "CGPA : " << student1.getCGPA() << endl;
 }
-*/
+
 
 // W2 Q4
-/*
 struct MovieData{
     string title, director;
     int year;
@@ -218,10 +217,10 @@ int main(){
     movie2.print();
     return 0;
 }
-*/
+
 
 // W2 Q5
-// /*
+
 class Line{
     private:
         double length;
@@ -241,10 +240,9 @@ int main(){
     cout << "Length of line : " << line.getLength() << endl;
     return 0;
 }
-// */
+ 
 
 // W2 Q6
-/*
 class Rectangle{
     private:
         double length, width, area;
@@ -280,10 +278,9 @@ int main(){
     cout << "Total area of the 3 rooms : " << kitchen.getArea() + bedroom.getArea() + den.getArea() << endl;
     return 0;
 }
-*/
+
 
 // W2 Q7
-/*
 struct Weather{
     double totalRainfall, highTemp, lowTemp, averageTemp;
 };
@@ -336,10 +333,9 @@ int main(){
     cout << "Min rainfall : " << minRainfall << " in month " << minRainfallMonth << endl;
     cout << "Average monthly temperature : " << totalAverageTemp << endl;
 }
-*/
+
 
 // W2 Q8
-/*
 class Date{
     private:
         int day, month, year;
@@ -374,4 +370,373 @@ int main(){
     Date date(day,month,year);
     date.print();
 }
-*/
+
+
+// ---------------------------------- Week 3 ----------------------------------
+// W3 Q1
+class Instructor{
+    private:
+        string first_name, last_name;
+        int office_number;
+    public:
+        Instructor() {}
+        
+        void setValues(string first, string last, int office){
+            first_name = first;
+            last_name = last;
+            office_number = office;
+        }
+
+        void display() const{
+            cout << "Instructor: " << first_name << " " << last_name << endl;
+            cout << "Office number: " << office_number << endl;
+        }
+};
+
+class Classroom{
+    private:
+        int building_number;
+        int room_number;
+    public:
+        Classroom(){}
+
+        void setValues(int building, int room){
+            building_number = building;
+            room_number = room;
+        }
+
+        void display() const{
+            cout << "Building number: " << building_number << endl;
+            cout << "Room number: " << room_number << endl;
+        }
+};
+
+class CollegeCourse{
+    private:
+        Instructor ins;
+        Classroom class1;
+        int number_of_credits;
+    public:      
+        CollegeCourse(int credits) : number_of_credits(credits){}
+
+        void setValues(string first, string last, int office, int building, int room){
+            ins.setValues(first, last, office);
+            class1.setValues(building, room);
+        }
+
+        void display() const{
+            ins.display();
+            class1.display();
+            cout << "Number of credits: " << number_of_credits << endl;
+        }
+};
+
+int main(){
+    CollegeCourse course1(1), course2(2);
+    course1.setValues("Chi", "Hao", 123, 456, 789);
+    course2.setValues("Chi", "Hao", 123, 456, 789);
+
+    cout << endl << "Displaying course1: " << endl;
+    course1.display();
+    cout << endl << "Displaying course2: " << endl;
+    course2.display();
+
+    return 0;
+}
+
+// W3 Q2
+class Bank{
+    public:
+        double getBalance() {return 0;}
+};
+
+class BankA : public Bank{
+    private:
+        double balance;
+    public:
+        BankA(double b): balance(b) {}
+        double getBalance() {return balance;}
+};
+
+class BankB : public Bank{
+    private:
+        double balance;
+    public:
+        BankB(double b): balance(b) {}
+        double getBalance() {return balance;}
+};
+
+class BankC : public Bank{
+    private:
+        double balance;
+    public:
+        BankC(double b): balance(b) {}
+        double getBalance() {return balance;}
+};
+
+int main(){
+    BankA bankA(2000);
+    BankB bankB(5000);
+    BankC bankC(4000);
+
+    cout << "Balance of bank A : " << bankA.getBalance() << endl;
+    cout << "Balance of bank B : " << bankB.getBalance() << endl;
+    cout << "Balance of bank C : " << bankC.getBalance() << endl;
+
+    return 0;
+}
+
+// ---------------------------------- Week 4 ----------------------------------
+// Inheritance and composition
+
+// Q1 part 1
+class Shape{
+    private:
+        double height, width;
+    protected:
+        double length;
+    public:
+        Shape(){}
+        Shape(double h, double w, double l) : height(h), width(w), length(l){}
+        void print() const{
+            cout << "Height: " << height << endl;
+            cout << "Width: " << width << endl;
+            cout << "Length: " << length << endl;
+        }
+        void setVal(double h, double w, double l){
+            height = h;
+            width = w;
+            length = l;
+        }
+
+        void getHW(double &h, double &w){     // new trick : getter for height and width
+            h = height;
+            w = width;
+        }
+
+    
+        ~Shape(){}
+};
+
+// Q1 part 2
+class Prism : public Shape{
+    private:
+        double volume;
+    public:
+        Prism() : Shape() { volume = 0; }
+        Prism(double h,double w, double l, double v)
+            :Shape(h,w,l){
+                volume = v;
+            }
+        void printPrism() const{ // use different name so that print() in Shape class is not overidden
+            print();  // 
+            cout << "Volume: " << volume << endl;
+        }
+        void setDimension(double l, double w, double h){
+            setVal(h,w,l);
+        }
+        void calculateVolume(){
+            double h,w;          // use the getter this way
+            getHW(h,w);
+            volume = h * w * length;
+        }
+
+        ~Prism(){}
+};
+
+// Q1 part 3
+class Cube{
+    private:
+        double volume;
+        Shape shape;
+    public:
+        Cube() : shape() { volume = 0; }
+        void print() const{
+            shape.print();
+            cout << "Volume: " << volume << endl;
+        }
+        void setDimension(double l, double w, double h){
+            shape.setVal(h,w,l);
+        }
+        void calculateVolume(){
+            double h,w;
+            shape.getHW(h,w);
+            volume = h * h * w;
+        }
+        ~Cube(){}
+};
+
+int main(){
+    cout << "Shape:" << endl;
+    Shape s1(5, 3, 8);
+    s1.print();
+
+    double h, w;
+    s1.getHW(h,w);
+    cout << endl << "Height: " << h << endl;
+    cout << "Width: " << w << endl << endl;
+    s1.setVal(1,2,3);
+    s1.print();
+
+    cout << endl << "Prism:" << endl;
+    Prism p1(1,2,3,4);
+    p1.printPrism();
+    p1.setDimension(5,6,7);
+    p1.printPrism();
+    p1.calculateVolume();
+
+    cout << endl << "Cube:" << endl;
+    Cube c1;
+    c1.setDimension(1,2,3);
+    c1.print();
+    c1.setDimension(4,5,6);
+    c1.calculateVolume();
+    c1.print();
+
+    return 0;
+}
+// ----------------------------------------------------------------------------
+// Q2 part 1
+class Circle{
+    private:
+        double radian, degree, area;
+        const double PI = 3.1415;
+    protected:
+        double radius;
+    public:
+        Circle() : radius(0), radian(0), degree(0), area(0) {}
+        Circle(double radius, double degree) : radius(radius), degree(degree) {}
+        void print(){
+            calcArea();  // remember calculate before print
+            calcRadian();
+            cout << "Radius: " << radius << endl;
+            cout << "Radian: " << radian << endl;
+            cout << "Degree: " << degree << endl;
+            cout << "Area: " << area << endl;
+        }
+        void setVal(double radius, double degree){
+            this->radius = radius;
+            this->degree = degree;
+            calcArea();
+            calcRadian();
+        }
+
+        double getRadius() const {return radius;} // accessor must be const otherwise cannot be accessed in derived class
+        double getDegree() const {return degree;}
+        double getPI() const {return PI;}
+        double getRadian(){
+            calcRadian();
+            return radian;
+        }
+        double getArea(){
+            calcArea();
+            return area;
+        }
+
+        void calcRadian(){
+            radian = degree * PI / 180;
+        }
+
+        void calcArea(){
+            area = PI * radius * radius;
+        }
+
+        ~Circle(){}
+};
+
+// Q2 part 2
+class Cone : public Circle{
+    private:
+        double height, area, volume;
+    public:
+        Cone(): Circle(), height(0), area(0), volume(0) {}
+        Cone(double height, double radius) 
+            : Circle(radius,0), height(height) {}
+
+        void print(){
+            calcArea();
+            calcVol();
+            cout << "Height: " << height << endl;
+            cout << "Radius: " << radius << endl;
+            cout << "Area: " << area << endl;
+            cout << "Volume: " << volume << endl;
+        }
+        void setDimension(double radius, double height){
+            this->radius = radius;
+            this->height = height;
+        }
+        void calcArea(){
+            area = getPI() * radius * (radius + sqrt(height*height + radius*radius));
+        }
+
+        void calcVol(){
+            volume = getPI() * radius * radius * height;
+        }
+
+        ~Cone(){}
+};
+
+// Q2 part 3
+class Cylinder{
+    private:
+        double volume, height;
+        Circle circle;
+    public:
+        Cylinder(): volume(0) {}
+        void print(){
+            calculateVol();
+            cout << "Radius: " << circle.getRadius() << endl;
+            cout << "height: " << height << endl;
+            cout << "Area: " << (2*circle.getPI()*pow(circle.getRadius(),2)) + (2*circle.getPI()*circle.getRadius()*height) << endl;
+            cout << "Volume: " << volume << endl;
+        }
+
+        void setDimension(double radius, double height){
+            circle.setVal(radius,0);
+            this->height = height;
+        }
+
+        void calculateVol(){
+            volume = circle.getPI()*pow(circle.getRadius(),2)*height;
+        }
+
+        ~Cylinder(){}
+};
+
+// Q2 part 4
+class Ellipse: public Circle{
+    private:
+        double shortRadius;
+    public:
+        Ellipse(): Circle(), shortRadius(0) {}
+        Ellipse(double radius, double shortRadius)
+            : Circle(radius,0), shortRadius(shortRadius) {}
+        double calcArea(){
+            return getPI()*radius*shortRadius;
+        }
+};
+
+int main(){
+    cout << "Circle" << endl;
+    Circle circle;
+    circle.setVal(5,0);
+    circle.print();
+    cout << "---------------------------------" << endl;
+    cout << "Cone" << endl;
+    Cone cone;
+    cone.setDimension(5,10);
+    cone.calcArea();
+    cone.calcVol();
+    cone.print();
+    cout << "---------------------------------" << endl;
+    cout << "Cylinder" << endl;
+    Cylinder cylinder;
+    cylinder.setDimension(5,10);
+    cylinder.calculateVol();
+    cylinder.print();
+    cout << "---------------------------------" << endl;
+    cout << "Ellipse" << endl;
+    Ellipse ellipse(5,3);
+    cout << "Area: " << ellipse.calcArea() << endl;
+    return 0;
+}
